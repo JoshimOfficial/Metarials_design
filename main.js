5e920244f6edfa9c1e6b8e23b2875b00556a7b76
Joshim Uddin <JoshimUddin01>
2022-09-18 10:39:43 +0600

    search added and dark fixed

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 let select_html = document.querySelector("html");
 
-let toggle_btn = document.querySelector(".toggle");
+let toggle_btn = document.querySelector("#toggle");
 toggle_btn.addEventListener("click",()=>{
     select_html.classList.toggle("dark")
 })
