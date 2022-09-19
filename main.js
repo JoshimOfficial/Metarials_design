09109dda32fa3802a6f72f33d21aaaa3489403c2
Joshim Uddin <JoshimUddin01>
2022-09-19 22:35:17 +0600

    added some design

--- main.js
+++ main.js
@@ -1,30 +1,26 @@
 //darkmode toggle
-
 let select_html = document.querySelector("html");
-
 let toggle_btn = document.querySelector("#toggle");
+let light_mode = document.querySelector("#light_mode");
+
 toggle_btn.addEventListener("click",()=>{
     select_html.classList.toggle("dark")
+
     if(select_html.classList[1]=='dark') {
-        document.cookie = "dark_mode=true";
+        window.localStorage.setItem("dark_mode", "true");
     }
     else {
-        document.cookie = "dark_mode=true; expires=Sat, 20 Jan 1980 12:00:00 UTC";
+       window.localStorage.removeItem("dark_mode")
     }
 })
 
-function getCookie(cookieName) {
-    let cookie = {};
-    document.cookie.split(';').forEach(function(el) {
-      let [key,value] = el.split('=');
-      cookie[key.trim()] = value;
-    })
-    return cookie[cookieName];
-  }
+let is_dark = window.localStorage.getItem("dark_mode");
 
-if(getCookie('dark_mode')=='true') {
-    select_html.classList.add("dark");
+if(is_dark) {
+    window.localStorage.setItem("dark_mode", "true");
+    select_html.classList.add("dark")
 }
 else {
-    select_html.classList.remove("dark")
+    window.localStorage.removeItem("dark_mode")
+    select_html.classList.remove("dark");
 }
